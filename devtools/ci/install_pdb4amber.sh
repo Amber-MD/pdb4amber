bae5450d3b108ff27958ba57a6a54e176ce4c10d
Hai Nguyen <hainm@users.noreply.github.com>
2020-06-12 00:24:06 -0400

    Update setup_env.sh (#84)

--- devtools/ci/install_pdb4amber.sh
+++ devtools/ci/install_pdb4amber.sh
@@ -2,4 +2,4 @@
 
 # change folder to avoid remove source code. ack.
 (cd tests && rm -rf `python -c "import pdb4amber; import os; print(os.path.dirname(pdb4amber.__file__))"`)
-python setup.py install
+pip install .
--- devtools/ci/setup_env.sh
+++ devtools/ci/setup_env.sh
@@ -21,7 +21,7 @@ conda install scipy -y
 conda install coverage -y 
 # conda install libgfortran -c conda-forge -y
 
-conda install ambertools=18 -c http://ambermd.org/downloads/ambertools/conda/ -y
+conda install ambertools -c conda-forge -y
 pip install mock
 
 # pytest
